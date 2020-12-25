4b6a24f5d5d9bd682bbabec5969155988dd81c1b
unknown <bsmithx479x@gmail.com>
2020-12-24 20:31:11 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#SKYN').fadeIn(1000).removeClass('#SKYN');
+});
+
+$(document).ready(function() {
+    $('div.dream').fadeIn(3000).removeClass('dream');
+});
+
+$(document).ready(function() {
+    $('div.create').fadeIn(3500).removeClass('create');
+});
+
+$(document).ready(function() {
+    $('div.inspire').fadeIn(4500).removeClass('inspire');
+});
